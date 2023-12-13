066d0bb1338e74cc1be9becb22425624bf5e498f
lemquan1001 <leminhquan.061001@gmail.com>
2023-12-13 17:09:03 +0700

    screen manage customer

--- src/main/java/com/example/ecommerce_be/repositories/BillRepository.java
+++ src/main/java/com/example/ecommerce_be/repositories/BillRepository.java
@@ -1,20 +1,17 @@
 package com.example.ecommerce_be.repositories;
 
 import com.example.ecommerce_be.entity.Bill;
-import com.example.ecommerce_be.entity.Category;
-import com.example.ecommerce_be.entity.Product_T;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface BillRepository extends JpaRepository<Bill, Long> {
     @Query(value ="SELECT p.* from bill p where p.status=0", nativeQuery = true)
     List<Bill> getAllBill();
-    @Query(value ="SELECT p.* from bill p where p.email = ?1 ", nativeQuery = true)
+    @Query(value ="SELECT p.* from bill p where p.id = :id ", nativeQuery = true)
     List<Bill> getBillById(String id);
 
     @Query(value ="SELECT b.* from bill b where b.status = 0 ", nativeQuery = true)
@@ -28,6 +25,9 @@ public interface BillRepository extends JpaRepository<Bill, Long> {
 
     @Query(value ="SELECT b.* from bill b where b.status = 3 ", nativeQuery = true)
     List<Bill> getBillByStatus3();
+
+    @Query(value ="SELECT c.brand,c.description,c.product_name,b.size,a.quantify,c.price from bill_detail a,product_details b,products c where a.product_detail_id = b.id and b.product_code =c.id and a.billid = :id ", nativeQuery = true)
+    List<Bill> getInfoProOfCusById(Long id);
 //    @Query(value ="SELECT p.* from products p where p.id = ?1", nativeQuery = true)
 //    Optional<Product_T> getProduct_ById(Long id);
 //    @Query(value ="SELECT p.* from products p where p.product_name like %?1%", nativeQuery = true)
--- src/main/java/com/example/ecommerce_be/service/BillService.java
+++ src/main/java/com/example/ecommerce_be/service/BillService.java
@@ -2,6 +2,7 @@ package com.example.ecommerce_be.service;
 
 import com.example.ecommerce_be.dto.BillDTO;
 import com.example.ecommerce_be.dto.Product_DTO;
+import com.example.ecommerce_be.entity.Bill;
 import com.example.ecommerce_be.entity.Category;
 
 import java.util.List;
@@ -16,6 +17,8 @@ public interface BillService {
     List<BillDTO> getBillByStatus3();
     BillDTO addNewBill(BillDTO billDTO);
     List<BillDTO> getBillById(String id);
+    public List<BillDTO> getInforById(Long id);
+    public BillDTO updateBill(BillDTO billDTO);
 
 
 }
